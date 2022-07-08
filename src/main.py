68d4d7c4c0d38bbea5ca8bc526042ac76d2e55d5
Sulthan Abiyyu <zblackjack980@gmail.com>
2022-07-08 19:03:44 +0700

    keyword based evaluation

--- src/main.py
+++ src/main.py
@@ -11,8 +11,6 @@ def main():
     # print(image_to_label_ner(["./data/raw/kucing.jpg"]))
     # print(image_to_label_keywords(["./data/raw/kucing.jpg"], ["cat", "dog"]))
 
-    # Diberi pilihan, mau based on keyword atau NER
-
     # Test keyword based prediction
     cat_images_path = []
     dog_images_path = []
@@ -22,20 +20,33 @@ def main():
     for file in glob.glob("data\\raw\\dog\\*.jpeg"):
         dog_images_path.append(file)
 
+    cat_label = []
+    dog_label = []
+
     print(f"Starting to predict 🐱 with {len(cat_images_path)} images...")
     cat_start_time = time.time()
-    cat_label = image_to_label_keywords(cat_images_path, ["cat"])
+    for file in cat_images_path:
+        cat_label.append(image_to_label_keywords([file], ["cat"]))
     print("Done predicting 🐱 in {} seconds".format(time.time() - cat_start_time))
 
     print(f"Starting to predict 🐶 with {len(dog_images_path)} images...")
     dog_start_time = time.time()
-    dog_label = image_to_label_keywords(dog_images_path, ["dog"])
+    for file in dog_images_path:
+        dog_label.append(image_to_label_keywords([file], ["dog"]))
     print("Done predicting 🐶 in {} seconds".format(time.time() - dog_start_time))
 
+    true_cat_pred = [cat for cat in cat_label if cat == "cat"]
+    true_dog_pred = [dog for dog in dog_label if dog == "dog"]
+
     print(
-        f"Cat unique label results : {pd.DataFrame(cat_label).value_counts()}")
+        f"Cat true prediction accuracy : {len(true_cat_pred) / len(cat_label) * 100}%")
     print(
-        f"Dog unique label results : {pd.DataFrame(dog_label).value_counts()}")
+        f"Dog true prediction accuracy : {len(true_dog_pred) / len(dog_label) * 100}%")
+
+    """
+    Cat true prediction accuracy : 94.5945945945946%
+    Dog true prediction accuracy : 94.87179487179486%
+    """
 
 
 if __name__ == "__main__":
