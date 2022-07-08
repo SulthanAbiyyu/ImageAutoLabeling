9ec432f2d2cde9c7774d766a6ecf5d4976f3eaa9
Sulthan Abiyyu <zblackjack980@gmail.com>
2022-07-08 19:14:59 +0700

    NER mode evaluation

--- src/main.py
+++ src/main.py
@@ -1,17 +1,11 @@
 import glob
 import time
 
-import pandas as pd
-
 from train import image_to_label_ner, image_to_label_keywords
 
 
-def main():
-    # print(predict_step(["./data/raw/kucing.jpg"]))
-    # print(image_to_label_ner(["./data/raw/kucing.jpg"]))
-    # print(image_to_label_keywords(["./data/raw/kucing.jpg"], ["cat", "dog"]))
+def main(mode="keywords"):
 
-    # Test keyword based prediction
     cat_images_path = []
     dog_images_path = []
     for file in glob.glob("data\\raw\\cat\\*.jpeg"):
@@ -23,31 +17,67 @@ def main():
     cat_label = []
     dog_label = []
 
-    print(f"Starting to predict 🐱 with {len(cat_images_path)} images...")
-    cat_start_time = time.time()
-    for file in cat_images_path:
-        cat_label.append(image_to_label_keywords([file], ["cat"]))
-    print("Done predicting 🐱 in {} seconds".format(time.time() - cat_start_time))
+    if mode == "keywords":
+        print("== Keywords mode ==")
+        print(f"Starting to predict 🐱 with {len(cat_images_path)} images...")
+        cat_start_time = time.time()
+        for file in cat_images_path:
+            cat_label.append(image_to_label_keywords([file], ["cat"]))
+        print("Done predicting 🐱 in {} seconds".format(
+            time.time() - cat_start_time))
 
-    print(f"Starting to predict 🐶 with {len(dog_images_path)} images...")
-    dog_start_time = time.time()
-    for file in dog_images_path:
-        dog_label.append(image_to_label_keywords([file], ["dog"]))
-    print("Done predicting 🐶 in {} seconds".format(time.time() - dog_start_time))
+        print(f"Starting to predict 🐶 with {len(dog_images_path)} images...")
+        dog_start_time = time.time()
+        for file in dog_images_path:
+            dog_label.append(image_to_label_keywords([file], ["dog"]))
+        print("Done predicting 🐶 in {} seconds".format(
+            time.time() - dog_start_time))
 
-    true_cat_pred = [cat for cat in cat_label if cat == "cat"]
-    true_dog_pred = [dog for dog in dog_label if dog == "dog"]
+        true_cat_pred = [cat for cat in cat_label if cat == "cat"]
+        true_dog_pred = [dog for dog in dog_label if dog == "dog"]
 
-    print(
-        f"Cat true prediction accuracy : {len(true_cat_pred) / len(cat_label) * 100}%")
-    print(
-        f"Dog true prediction accuracy : {len(true_dog_pred) / len(dog_label) * 100}%")
+        print(
+            f"Cat true prediction accuracy : {len(true_cat_pred) / len(cat_label) * 100}%")
+        print(
+            f"Dog true prediction accuracy : {len(true_dog_pred) / len(dog_label) * 100}%")
 
-    """
-    Cat true prediction accuracy : 94.5945945945946%
-    Dog true prediction accuracy : 94.87179487179486%
-    """
+        """
+        Done predicting 🐱 in 14.83539342880249 seconds
+        Cat true prediction accuracy : 94.5945945945946%
 
+        Done predicting 🐶 in 10.83300232887268 seconds
+        Dog true prediction accuracy : 94.87179487179486%
+        """
+    elif mode == "NER":
+        print("== NER mode ==")
+        print(f"Starting to predict 🐱 with {len(cat_images_path)} images...")
+        cat_start_time = time.time()
+        for file in cat_images_path:
+            cat_label.append(image_to_label_ner([file]))
+        print("Done predicting 🐱 in {} seconds".format(
+            time.time() - cat_start_time))
+
+        print(f"Starting to predict 🐶 with {len(dog_images_path)} images...")
+        dog_start_time = time.time()
+        for file in dog_images_path:
+            dog_label.append(image_to_label_ner([file]))
+        print("Done predicting 🐶 in {} seconds".format(
+            time.time() - dog_start_time))
+
+        true_cat_pred = [cat for cat in cat_label if cat == "cat"]
+        true_dog_pred = [dog for dog in dog_label if dog == "dog"]
+
+        print(
+            f"Cat true prediction accuracy : {len(true_cat_pred) / len(cat_label) * 100}%")
+        print(
+            f"Dog true prediction accuracy : {len(true_dog_pred) / len(dog_label) * 100}%")
+        """
+        Done predicting 🐱 in 27.089590311050415 seconds
+        Cat true prediction accuracy : 78.37837837837837%
+
+        Done predicting 🐶 in 23.333070039749146 seconds
+        Dog true prediction accuracy : 84.61538461538461%
+        """
 
 if __name__ == "__main__":
-    main()
+    main(mode="NER")
